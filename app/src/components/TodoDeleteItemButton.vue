0a570cd55b41cf6513130696cbb71655c00ddcd9
claudionhangapc <claudionhangapc@gmail.com>
2022-09-25 00:44:44 -0300

    created TodoDeleteItemButton component

---
+++ app/src/components/TodoDeleteItemButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-delete">
+    <span><img src="@/assets/icon-cross.svg" alt="" /></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoDeleteItemButton",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container-delete span {
+  cursor: pointer;
+  display: flex;
+  height: 20px;
+  width: 20px;
+  align-items: center;
+  justify-content: center;
+}
+.container-delete span img {
+  height: 15px;
+  width: 15px;
+}
+</style>
