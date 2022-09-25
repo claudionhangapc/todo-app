a527306b0839fb8376d0e368dedbd653019ddf09
claudionhangapc <claudionhangapc@gmail.com>
2022-09-25 00:47:02 -0300

    created TodoList.vue

---
+++ app/src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <TodoItem />
+    <TodoItem />
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+  name: "TodoList",
+  components: {
+    TodoItem,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+}
+</style>
