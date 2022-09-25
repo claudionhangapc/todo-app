95a594368971d53080bd6b52d286a423f7bd3399
claudionhangapc <claudionhangapc@gmail.com>
2022-09-24 22:03:41 -0300

    added TodoAdd component to TheHeader

--- app/src/components/TheHeader.vue
+++ app/src/components/TheHeader.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="header">
+  <div class="header bg-dark">
+    <div class="container">
+      <div class="content">
+        <h1>Todo</h1>
+        <span><img src="@/assets/icon-moon.svg" alt="" /></span>
+      </div>
+      <TodoAdd />
+    </div>
   </div>
 </template>
 
 <script>
+import TodoAdd from "./TodoAdd.vue";
 export default {
-  name: "HelloWorld",
+  name: "TheHeader",
+  components: {
+    TodoAdd,
+  },
   props: {
     msg: String,
   },
@@ -14,18 +25,36 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.header {
+  padding: 50px 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.bg-dark {
+  background-image: url("../assets/bg-desktop-dark.jpg");
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.bg-light {
+  background-image: url("../assets/bg-desktop-light.jpg");
 }
-a {
-  color: #42b983;
+
+.container {
+  max-width: 524px;
+  flex-direction: row;
+  margin: 0 auto;
+}
+
+.header .content {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.header .content h1 {
+  text-transform: uppercase;
+  font-size: 2.5rem;
+  color: #fafafa;
+  font-weight: 700;
 }
 </style>
