3e73775edca9ac2843d7237a504df83db15c70b2
claudionhangapc <claudionhangapc@gmail.com>
2022-09-24 21:57:12 -0300

    created component to add new task

---
+++ app/src/components/TodoAdd.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <span><img src="@/assets/icon-check.svg" alt="" /></span>
+    <div class="create-todo">
+      <input placeholder="Create a new todo..." />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoAdd",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 10px 20px;
+  position: relative;
+  transition: 0.3s;
+  background-color: #fff;
+  border-radius: 3px;
+}
+
+.container span {
+  border-radius: 50%;
+  cursor: pointer;
+  display: inline-block;
+  height: 20px;
+  width: 20px;
+  border: 1px solid #4d5066;
+  display: inline-block;
+  margin-right: 10px;
+}
+.container .create-todo {
+  display: flex;
+  flex: 1;
+}
+.container input {
+  border: none;
+  color: #484b6a;
+  font-size: 18px;
+  outline: none;
+  padding: 5px;
+  width: 100%;
+}
+</style>
