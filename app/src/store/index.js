53dd3494b19ba1d71fcda228ace41dcfcfad44bf
claudionhangapc <claudionhangapc@gmail.com>
2022-09-25 23:55:26 -0300

    started with store

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -4,9 +4,34 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    tasks: [],
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD(state, payload) {
+      state.tasks.push(payload);
+    },
+    SET(state, payload) {
+      state.tasks = payload;
+    },
+    DELETE(state, id) {
+      const index = state.tasks.findIndex((task) => task.id === id);
+      state.tasks.splice(index, 1);
+    },
+  },
+  actions: {
+    create({ commit }, payload) {
+      commit("ADD", payload);
+    },
+    async fetch({ commit }) {
+      const result = await fetch("http://localhost:3000/tasks");
+      const payload = await result.json();
+      commit("SET", payload);
+    },
+    delete({ commit }, id) {
+      commit("DELETE", id);
+    },
+  },
   modules: {},
 });
