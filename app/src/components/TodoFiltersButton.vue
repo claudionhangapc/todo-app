fc01cbca3c567df2eb637dccf99c0de99111c7a3
claudionhangapc <claudionhangapc@gmail.com>
2022-09-25 23:02:26 -0300

    created TodoFiltersButton

---
+++ app/src/components/TodoFiltersButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-filter">
+    <span v-for="filter in filters" :key="filter.id">{{ filter.name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoDeleteItemButton",
+  data() {
+    return {
+      filters: [
+        { id: "all", name: "All" },
+        { id: "active", name: "Active" },
+        { id: "completed", name: "Completed" },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container-filter span {
+  cursor: pointer;
+  margin: 0 10px;
+}
+</style>
