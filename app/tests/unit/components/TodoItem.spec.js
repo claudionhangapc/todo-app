7c24e974bd3690dba1d27706201ccd301bcd146a
claudionhangapc <claudionhangapc@gmail.com>
2022-09-27 02:42:39 -0300

    created first test

---
+++ app/tests/unit/components/TodoItem.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import TodoItem from "@/components/TodoItem.vue";
+import Vuex from 'vuex'
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+describe("TodoItem.vue", () => {
+
+  let  darkMode= false;
+  let store;
+  let state;
+
+  beforeEach(() => {
+    state = {
+      darkMode: darkMode
+    }
+    store = new Vuex.Store({
+      state
+    })
+  });
+
+  it("renders props.name when passed", () => {
+    const task = {
+      "id": 2,
+      "name": "Trabalaho de fisica",
+      "completed": 0
+    };
+    const wrapper = shallowMount(TodoItem, { store, localVue, propsData: { task } })
+    expect(wrapper.html()).toContain('Trabalaho de fisica');
+  });
+});
